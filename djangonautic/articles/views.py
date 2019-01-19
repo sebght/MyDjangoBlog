4c87bd67d04d3aa54a733bc786b7474bfda8d34a
Sebastien Gahat <seb.gahat@gmail.com>
2019-01-19 14:08:39 +0100

    add some article details in front

--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -8,4 +8,5 @@ def article_list(request):
     return render(request,'articles/article_list.html',{'articles':articles})
 
 def article_detail(request,slug):
-    return HttpResponse(slug)
+    article = Article.objects.get(slug=slug)
+    return render(request,'articles/article_detail.html',{'article':article})
